3e52b18e1b1227d2137069c59714eb5378614e88
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-07-31 13:51:52 -0400

    Updating Player Names

--- public/main.js
+++ public/main.js
@@ -1,1 +1,14 @@
+const updatePlayerOneName = () => {
+  console.log('updating player one name')
+  const playerName = document.querySelector('.p1-name-input').value
+  document.querySelector('#playerOneName').textContent = playerName
+}
 
+const updatePlayerTwoName = () => {
+  console.log('updating player one name')
+  const playerName = document.querySelector('.p2-name-input').value
+  document.querySelector('#playerTwoName').textContent = playerName
+}
+
+document.querySelector('.change-p1-button').addEventListener('click', updatePlayerOneName)
+document.querySelector('.change-p2-button').addEventListener('click', updatePlayerTwoName)
